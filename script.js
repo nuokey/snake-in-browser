37e5c3015cc42c10fd3b3e62c13ad79e01e04657
Timo <66961498+nuokey@users.noreply.github.com>
2022-02-24 15:23:54 +0300

    Update

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ let apple = [30, 30]
 
 let canvas = document.getElementById('canvas');
 
+canvas.width = screen.width;
+canvas.height = screen.height * 0.6;
+
 let speedx = 1;
 let speedy = 0;
 let speed = 5;
@@ -38,45 +41,90 @@ function move() {
     }
 }
 
-function play(e) {
-    if (e.key == "w") {
-        if (speedy == 0) {
-            speedx = 0;
-            speedy = -1;
-        }
+function turnUp() {
+    if (speedy == 0) {
+        speedx = 0;
+        speedy = -1;
     }
-    if (e.key == "s") {
-        if (speedy == 0) {
-            speedx = 0;
-            speedy = 1;
-        }
+}
+function turnDown() {
+    if (speedy == 0) {
+        speedx = 0;
+        speedy = 1;
     }
-    if (e.key == "a") {
-        if (speedx == 0) {
-            speedx = -1;
-            speedy = 0;
-        }
+}
+
+function turnLeft() {
+    if (speedx == 0) {
+        speedx = -1;
+        speedy = 0;
     }
-    if (e.key == "d") {
-        if (speedx == 0) {
-            speedx = 1;
-            speedy = 0;
-        }
+}
+
+function turnRight() {
+    if (speedx == 0) {
+        speedx = 1;
+        speedy = 0;
+    }
+}
+
+function keyboardCheck(e) {
+    console.log(e.key);
+    if (e.key == "ArrowUp") {
+        turnUp();
+    }
+    if (e.key == "ArrowDown") {
+        turnDown();
+    }
+    if (e.key == "ArrowLeft") {
+        turnLeft();
+    }
+    if (e.key == "ArrowRight") {
+        turnRight();
     }
 }
 
 function appleUpdate() {
     let distance = Math.sqrt((apple[0] + 10 - snake[0][0] - 5)**2 + (apple[1] + 10 - snake[0][1])**2 - 5);
-    if (distance <= 15 ) {
+    if (distance <= 20 ) {
         snake.push([snake[0][0], snake[0][1]]);
         apple[0] = Math.random() * (canvas.width - 20);
         apple[1] = Math.random() * (canvas.height - 20);
     }
 }
 
+function outOfBounds() {
+    if (snake[0][0] > canvas.width) {
+        snake[0][0] = -11;
+    }
+    if (snake[0][0] < -11) {
+        snake[0][0] = canvas.width;
+    }
+    if (snake[0][1] > canvas.height) {
+        snake[0][1] = -11;
+    }
+    if (snake[0][1] < -11) {
+        snake[0][1] = canvas.height;
+    }
+}
+
+function selfBite() {
+    for (let z = 1; i < snake.length; i++) {
+        let snakeDistance = Math.sqrt((snake[i][0] + 10 - snake[0][0] - 10)**2 + (snake[i][1] + 10 - snake[0][1])**2 - 10);
+        if (snakeDistance <= 5) {
+            for (let c = z; z < snake.length; i++) {
+                snake.pop(z)
+            }
+            break
+        }
+    }
+}
+
 function update() {
     move();
     appleUpdate();
+    outOfBounds();
+    // selfBite();
 
     draw();
 }
@@ -84,4 +132,4 @@ function update() {
 setInterval(update, 10);
 
 
-addEventListener("keydown", play);
+addEventListener("keydown", keyboardCheck);
